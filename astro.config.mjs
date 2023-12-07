1df2a086ab6c9a75cc7cff78a4bc53ba9d870556
rifatalam <rifat.alam@monstar-lab.com>
2023-12-07 17:29:01 +0600

    Basic astro with supabase

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astro-supabase-auth.vercel.app",
+  output: "hybrid",
+  adapter: cloudflare(),
+  integrations: [tailwind(), solidJs()],
+});
