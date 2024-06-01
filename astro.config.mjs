fa20901cf49d0ad47fe00a4f9bee6a6814650ad3
Tuo Nome <double.facessss@gmail.com>
2024-06-01 21:05:33 +0200

    tested vue component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://astro-supabase-70m.pages.dev',
-  integrations: [tailwind(), solidJs()],
+  site: 'https://astro-supabase-70m.pages.dev',
+  integrations: [
+    tailwind(), 
+    solidJs(), 
+    vue()
+  ],
   output: "server",
   adapter: cloudflare()
 });
